0867550f358de70ce33e6a144f6956231dd3c486
Michael Keplinger <michael@smashbrand.com>
2013-02-20 22:38:51 +0000

    original header

---
+++ html/inc/header.php
@@ -0,0 +1,43 @@
+<header class="gradient">
+
+	<div class="container-fluid">
+	
+		<a class="header-logo" href="index.php">Homepage logo</a>
+		
+		<div class="header-directory-options">
+		
+			<span class="header-directory-title">Directory:</span>							
+			
+			<span class="header-directory-current">
+								
+				<span class="dropdown">
+				  
+  					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Real Estate Pros <img src="assets/images/header/drop-down.png" alt="drop-down"></a>
+					  
+					<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+						<li><a tabindex="-1" href="#">Retail Space Users</a></li>
+					</ul>
+				  
+				</span><!-- .dropdown -->									
+			
+			</span><!-- .header-directory-current -->
+		
+		</div><!-- .header-directory-options -->
+		
+		<div class="header-account-functions">
+		
+			<ul>
+				
+				<li class="header-account-functions-name"><span>Matthew</span></li>
+				
+				<li class="header-account-functions-account"><span><a href="#">My Account</a></span></li>
+				
+				<li class="header-account-functions-logout"><a href="#">Logout</a></li>
+				
+			</ul>
+		
+		</div><!-- .header-account-functions -->
+	
+	</div><!-- .container-fluid -->
+
+</header>
